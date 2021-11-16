01546a3352d6d18548131dbd455ea221189687e1
Joachim Martillo <Joachim.CS.Martillo@gmail.com>
2021-11-16 01:41:44 -0500

    about to create child components. changed email and user name.

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -79,6 +79,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const decrementedIndex = index - 1;
@@ -103,6 +104,7 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
